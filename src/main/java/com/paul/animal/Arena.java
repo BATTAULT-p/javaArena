2b9dec7c126dfea04f20d3d7b6f3f76042a3f854
Neteroso <98899638+Neteroso@users.noreply.github.com>
2022-05-19 10:24:55 +0200

    exercice 1 pour Mathieu

--- src/main/java/com/paul/animal/Arena.java
+++ src/main/java/com/paul/animal/Arena.java
@@ -7,35 +7,19 @@ public class Arena {
         Animal carapuce = new Animal("carapuce");
         Animal salameche = new Animal("salameche");
         Animal pikachu = new Animal("pikachu");
+        Fight match1 = new Fight();
 
         for (int combat = 0; combat<5;combat++) {
-            fight(carapuce, pikachu);
+
+            match1.match(carapuce, salameche);
         }
-        win(carapuce, pikachu);
+        win(carapuce, salameche);
     }
 
-    static void Fightwinner(Animal pokemon){
-        System.out.println("vainqueur :" + pokemon.getType() );
-        pokemon.win();
-    }
     static void Bigwinner(Animal pokemon){
         System.out.println("Le grand vainqueur est :" + pokemon.getType());
     }
-    static int randomNumber(){
-        return  (int) ((Math.random() * (10-1)));
-    }
-    static void fight(Animal fighter1, Animal fighter2){
-        int i = randomNumber() + fighter1.getPower();
-        int e = randomNumber() + fighter2.getPower();
 
-        Animal winner;
-        if (i > e) {
-            winner = fighter1;
-        } else {
-            winner = fighter2;
-        }
-        Fightwinner(winner);
-    }
     static void win(Animal fighter1, Animal fighter2){
         Animal bigwinner ;
         if (fighter1.getVictory() > fighter2.getVictory()) {
---
+++ src/main/java/com/paul/animal/Fight.java
@@ -0,0 +1,25 @@
+package com.paul.animal;
+
+public class Fight {
+
+    public void match(Animal fighter1, Animal fighter2) {
+        int finalPowerFighter1 = randomPower() + fighter1.getPower();
+        int finalPowerFighter2 = randomPower() + fighter2.getPower();
+
+        Animal winner;
+        if (finalPowerFighter1 > finalPowerFighter2) {
+            winner = fighter1;
+        } else {
+            winner = fighter2;
+        }
+        Fightwinner(winner);
+    }
+    static int randomPower(){
+
+        return  (int) ((Math.random() * (10-1)));
+    }
+    static void Fightwinner(Animal pokemon){
+        System.out.println("vainqueur :" + pokemon.getType() );
+        pokemon.win();
+    }
+}
